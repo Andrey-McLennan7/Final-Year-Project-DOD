e312e8033ae7e75bbec31e53bdd9afe754371645
TheVirtualGamer752 <andreymclennan.work@gmail.com>
2025-04-23 21:43:40 +0100

    Implemented a Job System in Invader Movement System

--- Assets/Scripts/Systems/InvaderMovementSystem.cs
+++ Assets/Scripts/Systems/InvaderMovementSystem.cs
@@ -2,38 +2,75 @@ using Unity.Entities;
 using Unity.Burst;
 using Unity.Transforms;
 using Unity.Mathematics;
+using Unity.Jobs;
 
 [BurstCompile]
 [UpdateAfter(typeof(CreateInvaderGridSystem))]
 partial struct InvaderMovementSystem : ISystem
 {
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
-        foreach ((RefRW<LocalTransform> localTransform, RefRO<Movement> movement, RefRO<Invader> invader) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Movement>, RefRO<Invader>>())
-        {
-            localTransform.ValueRW.Position +=
-                movement.ValueRO.movementDirection * movement.ValueRO.movementSpeed * SystemAPI.Time.DeltaTime;
+    private bool advanceRow;
 
-            if (movement.ValueRO.movementDirection.x > 0.0f && localTransform.ValueRO.Position.x >= ( 14.0f) ||
-                movement.ValueRO.movementDirection.x < 0.0f && localTransform.ValueRO.Position.x <= (-14.0f)  )
-            {
-                AdvanceRow(ref state);
-                break;
-            }
-        }
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        advanceRow = false;
     }
 
     [BurstCompile]
-    private void AdvanceRow(ref SystemState state)
+    public void OnUpdate(ref SystemState state)
     {
-        foreach ((RefRW<LocalTransform> localTransform, RefRW<Movement> movement, RefRO<Invader> invader) in SystemAPI.Query<RefRW<LocalTransform>, RefRW<Movement>, RefRO<Invader>>())
+        InvaderMovementJob movementJob = new InvaderMovementJob()
         {
-            movement.ValueRW.movementDirection.x *= -1.0f;
+            deltaTime = SystemAPI.Time.DeltaTime,
+        };
 
-            float3 position = localTransform.ValueRW.Position;
-            position.y -= 1.0f;
-            localTransform.ValueRW.Position = position;
-        }   
+        JobHandle movementHandle = movementJob.ScheduleParallel(state.Dependency);
+
+        state.Dependency = movementHandle;
+        movementHandle.Complete();
+
+        foreach ((RefRO<LocalTransform> localTransform, RefRO<Movement> movement, RefRO<Invader> invader) in SystemAPI.Query<RefRO<LocalTransform>, RefRO<Movement>, RefRO<Invader>>())
+        {
+            if (movement.ValueRO.movementDirection.x > 0.0f && localTransform.ValueRO.Position.x >=  (14.0f) ||
+                movement.ValueRO.movementDirection.x < 0.0f && localTransform.ValueRO.Position.x <= (-14.0f)  )
+            {
+                advanceRow = true;
+                break;
+            }
+        }
+
+        if (advanceRow)
+        {
+            AdvanceRowJob advanceRowJob = new AdvanceRowJob();
+            JobHandle advanceRowHandle = advanceRowJob.ScheduleParallel(state.Dependency);
+
+            state.Dependency = advanceRowHandle;
+
+            advanceRow = false;
+        }
+    }
+}
+
+[BurstCompile]
+public partial struct InvaderMovementJob : IJobEntity
+{
+    public float deltaTime;
+
+    public void Execute(ref LocalTransform localTransform, in Movement movement, in Invader invader)
+    {
+        localTransform.Position += movement.movementDirection * movement.movementSpeed * deltaTime;
+    }
+}
+
+[BurstCompile]
+public partial struct AdvanceRowJob : IJobEntity
+{
+    public void Execute(ref LocalTransform localTransform, ref Movement movement, in Invader invader)
+    {
+        movement.movementDirection.x *= -1.0f;
+
+        float3 position = localTransform.Position;
+        position.y -= 1.0f;
+        localTransform.Position = position;
     }
 }
